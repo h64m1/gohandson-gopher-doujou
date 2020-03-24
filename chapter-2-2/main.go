d7fec93f87a83e8bcb2f15a116f58f1e89fbeafe
mass8 <29457120+h64m1@users.noreply.github.com>
2020-03-24 22:49:58 +0900

    [mod] 2.2 定数、iota close #3
    - iotaで右辺の省略

--- chapter-2-2/main.go
+++ chapter-2-2/main.go
@@ -5,4 +5,26 @@ import "fmt"
 func main() {
 	const helloWorld1 = "Hello world !"
 	fmt.Println(helloWorld1)
+
+	// 右辺の省略
+	// 全部3
+	const (
+		a = 1 + 2
+		b
+		c
+	)
+	fmt.Println(a, b, c)
+
+	// iotaで1ずつincrement
+	const (
+		d = iota
+		e
+	)
+	// 式の中でも使える
+	const (
+		f = 1 << iota
+		g
+		h
+	)
+	fmt.Println(d, e, f, g, h)
 }
