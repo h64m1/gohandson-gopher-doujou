9110c71ed5805a06adec879d9c4e39bfbb0f114e
mass8 <29457120+h64m1@users.noreply.github.com>
2020-03-28 16:50:29 +0900

    [mod] switch文による実装を追加 #5
    - evenodd.goに関数を分離

--- chapter-2-4/evenodd/main.go
+++ chapter-2-4/evenodd/main.go
@@ -2,23 +2,6 @@ package main
 
 import "fmt"
 
-func evenOddStringIf(n int) string {
-	if n%2 == 0 {
-		return "偶数"
-	} else {
-		return "奇数"
-	}
-}
-
-func evenOddStringSwitch(n int) string {
-	switch {
-	case n%2 == 0:
-		return "偶数"
-	default:
-		return "奇数"
-	}
-}
-
 func main() {
 	// if
 	fmt.Println("--- ifで表示")
