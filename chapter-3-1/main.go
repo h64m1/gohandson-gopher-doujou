c6f0054339d8b97337fcf00be3adc447c4050faa
h64m1 <h64ms1@gmail.com>
2020-04-08 00:01:25 +0900

    [mod] 配列表示用メソッド #7

--- chapter-3-1/main.go
+++ chapter-3-1/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func existingType() {
 	// 組み込み型（数値）
 	var sum int
@@ -38,13 +40,37 @@ func existingType() {
 
 func compositType() {
 	// 配列
+	println("配列")
+	showArray()
+}
+
+func showArray() {
+	// 配列
 	// ゼロ値で初期化
 	var ns1 [5]int
+	printArray("ns1", ns1[0:5])
 
 	// 配列リテラルで初期化
 	var ns2 = [5]int{10, 20, 30, 40, 50}
+	printArray("ns2", ns2[0:5])
 
-	println(ns1[3], ns2[4])
+	// 要素数の値から型推論
+	ns3 := [...]int{60, 70, 80, 90, 100}
+	printArray("ns3", ns3[0:5])
+
+	// 6番目が50, 11番目が100, 他が0
+	ns4 := [...]int{5: 50, 10: 100}
+	printArray("ns4", ns4[0:11])
+
+	println(ns1[3], ns2[4], ns3[2], ns4[5])
+}
+
+func printArray(title string, array []int) {
+	print(title, ": ")
+	for i := 0; i < len(array); i++ {
+		fmt.Printf("%d ", array[i])
+	}
+	print("\n")
 }
 
 func main() {
