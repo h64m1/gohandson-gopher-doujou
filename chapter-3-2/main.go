7e6343a1b359c6778fb5bbf6798abfb34f3543de
h64m1 <h64ms1@gmail.com>
2020-10-13 22:35:16 +0900

    [mod] 多値の受け取り #10

--- chapter-3-2/main.go
+++ chapter-3-2/main.go
@@ -3,6 +3,9 @@ package main
 func main() {
 	// 組み込みの関数
 	printBuiltinFunction()
+
+	// 多値の受け取り
+	printMultipleReturnValues()
 }
 
 func printBuiltinFunction() {
@@ -15,4 +18,23 @@ func printBuiltinFunction() {
 	println("complex: 複素数型を作成")
 	println("imag/real: 複素数の虚部/実数部を取得")
 	println("panic/recover: パニックを起こす/回復する")
+}
+
+func printMultipleReturnValues() {
+	x, y := swap(10, 20)
+	println("swap (x,y)=(10,20):", x, y)
+	// 省略したい場合は _ (ブランク変数)
+	x, _ = swap(10, 20)
+	_, y = swap(10, 20)
+}
+
+func swap(x, y int) (int, int) {
+	return y, x
+}
+
+// 名前付き戻り値
+func swapNamedReturnValues(x, y int) (x2, y2 int) {
+	// 一時変数なしで値の入れ替え
+	y2, x2 = x, y
+	return
 }
