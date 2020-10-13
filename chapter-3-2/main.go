94381d0a0b8a8d96d25f1fa854d8a0baa8520fc8
mass8 <29457120+h64m1@users.noreply.github.com>
2020-10-13 22:42:14 +0900

    [mod] クロージャ #10

--- chapter-3-2/main.go
+++ chapter-3-2/main.go
@@ -1,11 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	// 組み込みの関数
 	printBuiltinFunction()
 
 	// 多値の受け取り
 	printMultipleReturnValues()
+
+	// 無名関数
+	printClosure()
 }
 
 func printBuiltinFunction() {
@@ -37,4 +42,32 @@ func swapNamedReturnValues(x, y int) (x2, y2 int) {
 	// 一時変数なしで値の入れ替え
 	y2, x2 = x, y
 	return
+}
+
+func printClosure() {
+	msg := "hello world"
+	func() {
+		// 関数の外のmsgを参照できる
+		println(msg)
+	}()
+
+	// 関数型
+	fs1 := make([]func() string, 2)
+	fs1[0] = func() string { return "hello" }
+	fs1[1] = func() string { return "world" }
+
+	for k, f := range fs1 {
+		fmt.Println(k, f())
+	}
+
+	// クロージャのよくあるバグ
+	// - 関数外の変数を参照している場合
+	// - 実行のタイミングでは値が変わっている可能性がある
+	fs2 := make([]func(), 3)
+	for i := range fs2 {
+		fs2[i] = func() { fmt.Println(i) }
+	}
+	for _, f := range fs2 {
+		f()
+	}
 }
