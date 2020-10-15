2a39b18855e04011b5ab9fee7097d89c29f6f42b
mass8 <29457120+h64m1@users.noreply.github.com>
2020-10-15 22:37:18 +0900

    [add] メソッド #12

---
+++ chapter-3-3/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+func main() {
+	// メソッド
+	method()
+
+	// レシーバ
+	receiver()
+
+	increment()
+}
+
+// メソッド: レシーバと紐付けられた関数
+// - データとそれに対する操作を紐付けるために用いる
+// - ドットでメソッドでアクセスする
+
+// Hex type
+type Hex int
+
+func (h Hex) String() string {
+	return fmt.Sprintf("%x", int(h))
+}
+
+func method() {
+	var hex Hex = 100
+	fmt.Println(hex.String())
+}
+
+// メソッドに関連付けられた変数
+// - メソッド呼び出し時には通常の引数と同じような扱いになる
+//   * コピーが発生
+// - ポインタを使うことでレシーバへの変更を呼び出し元に伝えることができる
+//   * レシーバがポインタの場合もドットでアクセスする
+
+// T type
+type T int
+
+func (t *T) f() { println("hi") }
+
+func receiver() {
+	var v T
+	(&v).f()
+	v.f()
+}
+
+// MyInt インクリメント用のタイプ
+type MyInt int
+
+func (n *MyInt) Inc() {
+	(*n)++
+}
+
+func increment() {
+	var n MyInt
+	println(n)
+	n.Inc()
+	println(n)
+}
