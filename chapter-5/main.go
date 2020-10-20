98fe18b6aa9fed41fcbcf7c2cec3d45327a9453f
mass8 <29457120+h64m1@users.noreply.github.com>
2020-10-20 22:11:13 +0900

    [mod] flagパッケージ #16

--- chapter-5/main.go
+++ chapter-5/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
+
+var msg = flag.String("msg", "デフォルト値", "説明")
+var n int
+
+func init() {
+	// ポインタを指定して設定を予約
+	flag.IntVar(&n, "n", 1, "回数")
+}
 
 func main() {
 	// os.Args: プログラム引数が入った文字列型のスライス
 	// 要素のひとつめはプログラム名
 	fmt.Println(os.Args)
+
+	// フラグパッケージ
+	flag.Parse()
+	fmt.Println(strings.Repeat(*msg, n))
+
+	// flag.Args関数はフラグの分は除外される
+	fmt.Println(flag.Args())
 }
