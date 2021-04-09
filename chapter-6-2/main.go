4cdcb94726eb3e3a0bb048da94a6d5cb15a1f321
mass8 <29457120+h64m1@users.noreply.github.com>
2021-04-09 19:18:59 +0900

    埋め込みの特徴 #18

--- chapter-6-2/main.go
+++ chapter-6-2/main.go
@@ -12,6 +12,19 @@ type Fuga struct {
 	Hoge // 名前のないフィールドになる
 }
 
+// 型リテラルでなければ埋め込められる
+// - typeで定義したものや組み込み型
+// - インターフェースも埋め込められる
+// Stringerを実装
+type Hex int
+
+func (h Hex) String() string {
+	return fmt.Sprintf("%x", int(h))
+}
+
+// Hex2もStringerを実装
+type Hex2 struct{ Hex }
+
 func main() {
 	f := Fuga{Hoge{N: 100}}
 
@@ -20,4 +33,7 @@ func main() {
 
 	// 型名を指定してアクセス出来る
 	fmt.Println(f.Hoge.N)
+
+	h := Hex2{10}
+	fmt.Println(h.String())
 }
